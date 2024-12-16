f560b20d30105855745f2960916280d24005a1b4
Anargyros Tsadimas <argiris@norbloc.com>
2024-12-16 23:49:26 +0200

    fixed keycloak params

--- api/main.py
+++ api/main.py
@@ -5,11 +5,53 @@ from db import init_db
 from routes.user import router as userrouter
 from routes.job import router as jobrouter
 from typing import Annotated
-
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from jwt import PyJWKClient
+import jwt
+from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080",
+    "http://keycloak:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+oauth_2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl="http://keycloak:8080/realms/cloudplatforms/protocol/openid-connect/token",
+    authorizationUrl="http://keycloak:8080/realms/cloudplatforms/protocol/openid-connect/auth",
+    refreshUrl="http://keycloak:8080/realms/cloudplatforms/protocol/openid-connect/token",
+)
+
+
+async def valid_access_token(
+    access_token: Annotated[str, Depends(oauth_2_scheme)]
+):
+    url = "http://keycloak:8080/realms/cloudplatforms/protocol/openid-connect/certs"
+    optional_custom_headers = {"User-agent": "custom-user-agent"}
+    jwks_client = PyJWKClient(url, headers=optional_custom_headers)
+
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience="api",
+            options={"verify_exp": True},
+        )
+        return data
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Not authenticated")
 
 
 @app.on_event("startup")
@@ -18,6 +60,9 @@ async def startup():
 
 
 
+@app.get("/private", dependencies=[Depends(valid_access_token)])
+def get_private():
+    return {"message": "Ce endpoint est privé"}
 
 app.include_router(userrouter, prefix="/user", tags=["User"])
 app.include_router(jobrouter, prefix="/job", tags=["Job"])
